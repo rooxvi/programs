04e6a47541170a6efc8c51d5a1eb53fe058fd54c
Ravichandran <ravichandran.s@saviynt.com>
2023-10-01 13:21:23 +0530

    Updated

--- src/main/java/io/ravi/programs/SecondLargestNumberInArray.java
+++ src/main/java/io/ravi/programs/SecondLargestNumberInArray.java
@@ -1,30 +1,25 @@
 package io.ravi.programs;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class SecondLargestNumberInArray {
     public static void main(String[] args) {
 
-        int[] array = {1,2,3,4,5,6};
+        int[] array = {-1,-3,1, 2, 3, 4, 6};
         System.out.println(findSecondLargestNumberInArray(array));
 
     }
 
     // Note: Not completed
-    private static int findSecondLargestNumberInArray(int[] array){
-        int firstMax = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        int start = 0;
-
-        while (start < array.length){
-          if(firstMax < array[start]){
-              firstMax = array[start];
-          }
-
-          if(secondMax < firstMax){
-             if (  secondMax < array[start]){
-                 secondMax = array[start];
-             }
-          }
-            start++;
+    private static int findSecondLargestNumberInArray(int[] array) {
+        PriorityQueue<Integer> integers = new PriorityQueue<>(Collections.reverseOrder());
+        for (int index = 0; index < array.length; index++) {
+            integers.add(array[index]);
+        }
+        int secondMax = -1;
+        for (int pop = 0; pop < 2; pop++) {
+            secondMax = integers.poll();
         }
         return secondMax;
     }
