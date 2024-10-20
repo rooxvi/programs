e82dfcea70c8d039e7b498f8c90063aee50c8aef
ravichandran <ravichandran.santhanam1@sony.com>
2024-10-20 08:59:11 +0530

    Commit works

---
+++ src/main/java/io/ravi/multithreading/printinsequence/PrintSequenceRunnableNThread.java
@@ -0,0 +1,57 @@
+package io.ravi.multithreading.printinsequence;
+
+public class PrintSequenceRunnableNThread implements Runnable {
+
+    private static Object lock = new Object();
+
+    private final int totalThreads;
+
+    private static int sequence = 1;
+
+    private final int reminder;
+
+    private static int flow = 0;
+
+    public PrintSequenceRunnableNThread(int totalThreads, int threadSequence) {
+        this.totalThreads = totalThreads;
+        this.reminder = threadSequence % totalThreads;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lock) {
+            System.out.println((flow++) + "." + Thread.currentThread().getName() + " acquired...");
+            while (sequence < 10) {
+                while (sequence % totalThreads != reminder) {
+                    System.out.println((flow++) + "." + Thread.currentThread().getName() + " waiting...");
+                    try {
+                        lock.wait();
+                        System.out.println((flow++) + "." + Thread.currentThread().getName() + " woke up..");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println((flow++) + "." + Thread.currentThread().getName() + " " + sequence);
+                sequence++;
+                lock.notifyAll();
+                System.out.println((flow++) + "." + Thread.currentThread().getName() + " " + "notified others....");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+  /*      int totalThreads = 3;
+        PrintSequenceRunnableNThread thread = new PrintSequenceRunnableNThread(totalThreads,1);
+        PrintSequenceRunnableNThread thread1 = new PrintSequenceRunnableNThread(totalThreads,2);
+        PrintSequenceRunnableNThread thread2 = new PrintSequenceRunnableNThread(totalThreads,3);
+
+        new Thread(thread).start();
+        new Thread(thread1).start();
+        new Thread(thread2).start();*/
+
+        int totalThreads = 5;
+        for (int i = 1; i <= totalThreads; i++) {
+            new Thread(new PrintSequenceRunnableNThread(totalThreads, i)).start();
+        }
+    }
+}
