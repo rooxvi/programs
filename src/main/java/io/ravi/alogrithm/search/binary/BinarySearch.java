f1fba73b9c0806d75e9ee084b23432abd3fb1055
ravichandran <ravichandran.santhanam1@sony.com>
2024-10-23 00:25:01 +0530

    Commit works

--- src/main/java/io/ravi/alogrithm/search/binary/BinarySearch.java
+++ src/main/java/io/ravi/alogrithm/search/binary/BinarySearch.java
@@ -1,26 +1,39 @@
 package io.ravi.alogrithm.search.binary;
 
-class BinarySearch {
+public class BinarySearch {
 
-  public static void main(String args[]) {
-    int[] array = {1,2,4,5,7,9};
-    int ans = binarySearch(array, 0, array.length - 1, 7);
-    System.out.println(ans);
-  }
+    public static void main(String args[]) {
+        int[] array = {1, 2, 4, 5, 7, 9};
+        // int ans = binarySearch(array, 0, array.length - 1, 7);
+        // System.out.println(ans);
 
-  private static int binarySearch(int[] arr,int low, int high, int k){
-    if(low <= high){
-      int midIndex = low + (high-low)/2;
-      if(arr[midIndex] == k)
-        return midIndex;
-      if(arr[midIndex] > k)
-        return binarySearch(arr,low,midIndex-1,k); // return statement is important
-      return binarySearch(arr,midIndex+1,high,k); // return statement is important
+        int indexAns = binarySearchIteration(array, 0, array.length - 1, 4);
+        System.out.println(indexAns);
     }
-    return -1;
-  }
 
-  //Response with status: 500 OK for URL: https://api.digiverifier.com/v1.0/candidate/getToken?invitationToken=0fa52bb173418e4d9f7d20bc263a683b4d678ca325c946d376f07cb80f84f2a1&otpResendFlag=true
+    private static int binarySearch(int[] arr, int low, int high, int k) {
+        if (low <= high) {
+            int midIndex = low + (high - low) / 2;
+            if (arr[midIndex] == k)
+                return midIndex;
+            if (arr[midIndex] > k)
+                return binarySearch(arr, low, midIndex - 1, k); // return statement is important
+            return binarySearch(arr, midIndex + 1, high, k); // return statement is important
+        }
+        return -1;
+    }
 
-
+    private static int binarySearchIteration(int[] arr, int low, int high, int k) {
+        while (low <= high) {
+            int midIndex = low + (high - low) / 2;
+            if (arr[midIndex] == k)
+                return midIndex;
+            if (arr[midIndex] > k) {
+                high = midIndex - 1;
+            } else {
+                low = midIndex + 1;
+            }
+        }
+        return -1;
+    }
 }
--- src/main/java/io/ravi/alogrithm/search/binary/SearchElementInRotatedArray.java
+++ src/main/java/io/ravi/alogrithm/search/binary/SearchElementInRotatedArray.java
@@ -1,29 +1,27 @@
 package io.ravi.alogrithm.search.binary;
 
-import java.util.HashMap;
-
 public class SearchElementInRotatedArray {
 
     public static void main(String[] args) {
-        int[] array = {4,5,7,1,3};
-        System.out.println(search(array,0,array.length-1,3));
+        int[] array = {4, 5, 7, 1, 3};
+        System.out.println(search(array, 0, array.length - 1, 3));
     }
 
-    private static int search(int[] array,int low,int high,int k){
-        while (low <= high){
-            int midIndex = low + (high - low)/2;
-            if(array[midIndex] == k) return midIndex;
-            if(array[low] <= array[midIndex]){
-                if (array[low] <= k && array [midIndex] <= k){
+    private static int search(int[] array, int low, int high, int k) {
+        while (low <= high) {
+            int midIndex = low + (high - low) / 2;
+            if (array[midIndex] == k) return midIndex;
+            if (array[low] <= array[midIndex]) {
+                if (array[low] <= k && array[midIndex] <= k) {
                     high = midIndex - 1;
-                }else {
-                    low = midIndex+1;
+                } else {
+                    low = midIndex + 1;
                 }
-            }else {
-                if(array[midIndex] <= k &&  k <= array[high]){
-                    low = midIndex +1;
-                }else {
-                    high = midIndex -1;
+            } else {
+                if (array[midIndex] <= k && k <= array[high]) {
+                    low = midIndex + 1;
+                } else {
+                    high = midIndex - 1;
                 }
             }
         }
